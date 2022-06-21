98baa07b73e65c7a47f3f054c9a4b34c0b3e4a90
Ankit Singh <singhankit62000@gmail.com>
2022-06-22 04:47:41 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUUzuhxAQ-0-TflEfOo7X1WQPnsfENgcs",
+    authDomain: "netflix-build-bb288.firebaseapp.com",
+    projectId: "netflix-build-bb288",
+    storageBucket: "netflix-build-bb288.appspot.com",
+    messagingSenderId: "405370472650",
+    appId: "1:405370472650:web:b3706035b3a35be6f78c33"
+  };
